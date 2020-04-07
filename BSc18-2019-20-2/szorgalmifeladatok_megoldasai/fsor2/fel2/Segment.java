1c364fd8b4a66f8d585c8621c1c23ea3b9a46413
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2020-04-07 19:03:47 +0200

    Upload lab2/exc2 homework solution
    new file:   BSc18-2019-20-2/szorgalmifeladatok_megoldasai/fsor2/fel2/Segment.java

---
+++ BSc18-2019-20-2/szorgalmifeladatok_megoldasai/fsor2/fel2/Segment.java
@@ -0,0 +1,37 @@
+class Segment {
+    double x1, y1, x2, y2;
+
+    Line line(){
+        Line line = new Line();
+        if( x1 == x2 ){
+            line.b = 1.0;
+            line.a = (y2 - y1) / (x1 - x2);
+        } else
+            line.a = 1.0;
+            line.b = (x1 - x2) / (y2 - y1);
+        }
+        line.c = line.a * x1 + line.b * y1;
+        return line;
+    }
+
+    boolean contains( Point p ){
+        return line().contains(p) && 
+               ((x1 <= p.x && p.x <= x2) || (x1 >= p.x && p.x >= x2) ) &&
+               ((y1 <= p.y && p.y <= y2) || (y1 >= p.y && p.y >= y2) ) ;
+    }
+
+    double orientation( Point p ){
+        return (y2 - y1)*(p.x - x2) - (p.y - y2)*(x2 - x1);
+    }
+
+    double intersects( Segment that ){
+        Line thisLine = this.line();
+        Line thatLine = that.line();
+        if( thisLine.isParallelWith( thatLine ) ){
+            return false;
+        } else {
+            Point intersection = thisLine.intersectionWith(thatLine);
+            return this.contains(intersection) && that.contains(intersection);
+        }
+    }
+}
