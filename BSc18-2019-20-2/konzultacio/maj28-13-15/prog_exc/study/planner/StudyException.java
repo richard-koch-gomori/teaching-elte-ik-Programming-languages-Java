9ceed804bc6d65baee93abb92ddc46f8664203e3
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2020-05-28 16:02:34 +0200

    Cons/4

--- BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/StudyException.java
+++ BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/StudyException.java
@@ -13,3 +13,4 @@ public class StudyException extends Exception
     }
 }
 
+
--- BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/test/StudyPlannerTest.java
+++ BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/test/StudyPlannerTest.java
@@ -2,6 +2,7 @@ package study.planner.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -25,13 +26,15 @@ public class StudyPlannerTest
         catch (StudyException e)
         {
             System.out.println(e.getMessage());
+            fail();
         }
         assertEquals(0, sp.getBookCount());
     }
 
     @Test
-    public void exampleBookCount()
+    public void exampleBookCount() throws StudyException
     {
+        /*
         StringBuilder sb = new StringBuilder();
         sb.append("4");
         sb.append(System.lineSeparator());
@@ -43,9 +46,23 @@ public class StudyPlannerTest
         sb.append(System.lineSeparator());
         sb.append("55 78 Programozasi Nyelvek Java");
         String text = sb.toString();
+        */
+
+        String text = 
+            "4" + 
+            System.lineSeparator() + 
+            "10 20 Programozasi Nyelvek Java" + 
+            System.lineSeparator() + 
+            "150 190 Analizis" + 
+            System.lineSeparator() + 
+            "20 130 Analizis" + 
+            System.lineSeparator() + 
+            "55 78 Programozasi Nyelvek Java";
+
         Scanner sc = new Scanner(text);
         StudyPlanner sp = new StudyPlanner();
 
+        /*
         try
         {
             sp.readPagesFromText(sc);
@@ -53,9 +70,76 @@ public class StudyPlannerTest
         catch (StudyException e)
         {
             System.out.println(e.getMessage());
+            fail();
         }
-
         assertEquals(2, sp.getBookCount());
+        */
+
+        sp.readPagesFromText(sc);
+        assertEquals(2, sp.getBookCount());
+    }
+
+    @Test
+    public void examplePageCount_Analizis() throws StudyException
+    {
+        String text = 
+            "4" + 
+            System.lineSeparator() + 
+            "10 20 Programozasi Nyelvek Java" + 
+            System.lineSeparator() + 
+            "150 190 Analizis" + 
+            System.lineSeparator() + 
+            "20 130 Analizis" + 
+            System.lineSeparator() + 
+            "55 78 Programozasi Nyelvek Java";
+
+        Scanner sc = new Scanner(text);
+        StudyPlanner sp = new StudyPlanner();
+
+        sp.readPagesFromText(sc);
+        assertEquals(152, sp.pageCountOf("Analizis"));
+    }
+
+    @Test
+    public void examplePageCount_Java() throws StudyException
+    {
+        String text = 
+            "4" + 
+            System.lineSeparator() + 
+            "10 20 Programozasi Nyelvek Java" + 
+            System.lineSeparator() + 
+            "150 190 Analizis" + 
+            System.lineSeparator() + 
+            "20 130 Analizis" + 
+            System.lineSeparator() + 
+            "55 78 Programozasi Nyelvek Java";
+
+        Scanner sc = new Scanner(text);
+        StudyPlanner sp = new StudyPlanner();
+
+        sp.readPagesFromText(sc);
+        assertEquals(35, sp.pageCountOf("Programozasi Nyelvek Java"));
+    }
+
+    @Test(expected = StudyException.class)
+    public void missingBook() throws StudyException
+    {
+        String text = 
+            "4" + 
+            System.lineSeparator() + 
+            "10 20 Programozasi Nyelvek Java" + 
+            System.lineSeparator() + 
+            "150 190 Analizis" + 
+            System.lineSeparator() + 
+            "20 130 Analizis" + 
+            System.lineSeparator() + 
+            "55 78 Programozasi Nyelvek Java";
+
+        Scanner sc = new Scanner(text);
+        StudyPlanner sp = new StudyPlanner();
+
+        sp.readPagesFromText(sc);
+        sp.pageCountOf("missingBook");
     }
 }
 
