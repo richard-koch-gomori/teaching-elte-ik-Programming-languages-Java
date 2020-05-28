471fd93aa3573cf4d9b776b0287500fee7b799d3
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2020-05-28 15:30:29 +0200

    Cons/4

--- BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/test/StudyPlannerTest.java
+++ BSc18-2019-20-2/konzultacio/maj28-13-15/prog_exc/study/planner/test/StudyPlannerTest.java
@@ -28,5 +28,34 @@ public class StudyPlannerTest
         }
         assertEquals(0, sp.getBookCount());
     }
+
+    @Test
+    public void exampleBookCount()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("4");
+        sb.append(System.lineSeparator());
+        sb.append("10 20 Programozasi Nyelvek Java");
+        sb.append(System.lineSeparator());
+        sb.append("150 190 Analizis");
+        sb.append(System.lineSeparator());
+        sb.append("20 130 Analizis");
+        sb.append(System.lineSeparator());
+        sb.append("55 78 Programozasi Nyelvek Java");
+        String text = sb.toString();
+        Scanner sc = new Scanner(text);
+        StudyPlanner sp = new StudyPlanner();
+
+        try
+        {
+            sp.readPagesFromText(sc);
+        }
+        catch (StudyException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        assertEquals(2, sp.getBookCount());
+    }
 }
 
