1241f8e09680e3fc1aeae7f33279f45409c01cd9
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2021-02-09 17:36:23 +0100

    Lab1/4

---
+++ BSc18-2020-21-2/gyakcsop-22/gyak01/Fel5.java
@@ -0,0 +1,13 @@
+
+class Fel5
+{
+    public static void main(String[] args)
+    {
+        for( int i = 1; i <= 4; i++ )
+        {
+            System.out.println(i/2.0);
+        }
+        System.out.println("Kiirtam 4 szamot");
+    }
+}
+
