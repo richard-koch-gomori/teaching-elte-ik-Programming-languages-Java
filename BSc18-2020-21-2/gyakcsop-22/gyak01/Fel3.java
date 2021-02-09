1b8c2c6fffcdd75d87315d0a6de1ba26da6507f6
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2021-02-09 17:20:46 +0100

    Lab1/2

---
+++ BSc18-2020-21-2/gyakcsop-22/gyak01/Fel3.java
@@ -0,0 +1,13 @@
+
+
+class Fel3
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter your name: ");
+        String name;
+        name = System.console().readLine();
+        System.out.println("Hello " + name);
+    }
+}
+
