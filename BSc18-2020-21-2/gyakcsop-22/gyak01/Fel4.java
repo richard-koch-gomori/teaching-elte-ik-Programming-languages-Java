eb706695227db035bceb9a103eb35e7e638883eb
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2021-02-09 17:29:44 +0100

    Lab1/3

---
+++ BSc18-2020-21-2/gyakcsop-22/gyak01/Fel4.java
@@ -0,0 +1,16 @@
+
+
+class Fel4
+{
+    public static void main(String[] args)
+    {
+        if (args.length >= 1)
+        {
+            System.out.println("Hello " + args[0] + "!");
+        }
+        else
+        {
+            System.out.println("Please give me your name");
+        }
+    }
+}
