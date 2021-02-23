951f91c547593f8b338b36106367c6aa029c4de8
Richárd Koch-Gömöri <kgomori.richard@gmail.com>
2021-02-23 17:17:01 +0100

    Lab03/1

---
+++ BSc18-2020-21-2/gyakcsop-22/gyak03/Fel1/README.md
@@ -0,0 +1,5 @@
+
+
+fordítás: `javac point2d/PointMain.java`  
+futtatás: `java point2d.PointMain`
+
---
+++ BSc18-2020-21-2/gyakcsop-22/gyak03/Fel1/point2d/Point.java
@@ -0,0 +1,27 @@
+package point2d;
+
+
+class Point
+{
+    int x;
+    int y;
+
+    void move(int dx, int dy)
+    {
+        x += dx;
+        y += dy;
+    }
+
+    void mirror(Point center)
+    {
+        x = 2*center.x - x;
+        y = 2*center.y - y;
+    }
+
+    double distance(Point p)
+    {
+        return Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2));
+    }
+};
+
+
